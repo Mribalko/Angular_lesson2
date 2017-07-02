f223409f80d70c01afd6a3991ec3778ccd32855c
Mribalko <mribalko@gmail.com>
2017-07-02 18:22:35 +0300

    Удален вывод отладочной информации

--- app/Account/AccountCtrl.js
+++ app/Account/AccountCtrl.js
@@ -5,13 +5,10 @@ angular
     .controller('AccountCtrl', function(AccountService) {
 
         var vm = this;
-        //vm.newPokemon = {};
 
         vm.account = AccountService.getAccount();
-        console.log(vm.account);
 
         vm.addAccountData = function() {
-            console.log(vm.account);
             AccountService.setAccount(vm.account);
         };
 
